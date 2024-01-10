5d40e2f5cdd407aecd070235c9f6d9f6171ec5ae
Devansh070805 <142911706+Devansh070805@users.noreply.github.com>
2024-01-10 06:00:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function clicksrch()
+{
+    let s=document.getElementById("sbar");
+        s.value="         ";
+}
+
+function check()
+{
+    // let s=document.getElementById("sbar");
+    // if(s.value=="         ")
+    // s.value="         Search Google or type a URL";
+}
